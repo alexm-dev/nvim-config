7cd00de8f943638506cc7f6dba045929692d76de
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2025-10-28 16:40:46 +0100

    Reworked highlights.lua to wrap nvim highlights set functions into local functions

--- lua/config/highlights.lua
+++ lua/config/highlights.lua
@@ -1,13 +1,37 @@
 -- Custom highlight overrides (direct, always applied)
 
--- C++ namespace type (example color: light gray)
-vim.api.nvim_set_hl(0, "@module.cpp", { fg = "#c8c8b9" })
+local function set_highlight()
+    -- C++ namespace type
+    vim.api.nvim_set_hl(0, "@module.cpp", { fg = "#c8c8b9" })
 
--- Class type
-vim.api.nvim_set_hl(0, "@type.cpp", { fg = "#A6E3A1" })
--- Pink for includes
-vim.api.nvim_set_hl(0, "@keyword.import.cpp", { fg = "#F5C2E7" })
--- Variable names in light pink
-vim.api.nvim_set_hl(0, "@variable.cpp", { fg = "#F5C2E7" })
--- Parameters in gray
-vim.api.nvim_set_hl(0, "@variable.parameter.cpp", { fg = "#9a9a9a" })
+    -- Class type
+    --vim.api.nvim_set_hl(0, "@type.cpp", { fg = "#A6E3A1" })
+
+    -- Constructor names in light blue (default method color)
+    vim.api.nvim_set_hl(0, "@constructor.cpp", { fg = "#89b4fa" })
+
+    -- Variable names in light pink
+    vim.api.nvim_set_hl(0, "@variable.cpp", { fg = "#F5C2E7" })
+
+    -- Parameters in gray
+    vim.api.nvim_set_hl(0, "@variable.parameter.cpp", { fg = "#9a9a9a" })
+end
+
+local function set_includes()
+    vim.api.nvim_set_hl(0, "@keyword.import", { fg = "#9a9a9a" })
+    vim.api.nvim_set_hl(0, "@keyword.import.cpp", { fg = "#9a9a9a" })
+end
+
+local function apply_highlights()
+    set_highlight()
+    set_includes()
+end
+
+-- apply immediately
+apply_highlights()
+
+-- re-apply after colorscheme changes so the color sticks
+vim.api.nvim_create_autocmd("ColorScheme", {
+    pattern = "*",
+    callback = apply_highlights,
+})
