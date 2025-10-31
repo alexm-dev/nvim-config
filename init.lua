cd40115f113d0942b8f9ae256c0388a4f34ec1d8
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2025-10-31 20:34:04 +0100

    Updated node.js version to 24
    Also removed lua_path and lua_cpath which helps with plugin latency

--- init.lua
+++ init.lua
@@ -1,16 +1,10 @@
 -- Node22 binaries to path
-if vim.env.LOCALAPPDATA and vim.loop.fs_stat(vim.env.LOCALAPPDATA .. "\\node22") then
-    vim.env.PATH = vim.env.LOCALAPPDATA .. "\\node22;" .. (vim.env.PATH or "")
+if vim.env.LOCALAPPDATA and vim.loop.fs_stat(vim.env.LOCALAPPDATA .. "\\node24") then
+    vim.env.PATH = vim.env.LOCALAPPDATA .. "\\node24;" .. (vim.env.PATH or "")
 end
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
--- Add LuaRocks paths to package.path and package.cpath
-local lr_path = vim.fn.system("luarocks path --lr-path"):gsub("\n", "")
-local lr_cpath = vim.fn.system("luarocks path --lr-cpath"):gsub("\n", "")
-package.path = package.path .. ";" .. lr_path
-package.cpath = package.cpath .. ";" .. lr_cpath
 
 -- Clangd settings
 require("config.clangd_settings")
