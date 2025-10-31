06037e1cf3dc515e5a97d15e371baf6be41c4eac
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2025-10-31 01:30:26 +0100

    Fixed plugins path

---
+++ lua/plugins/copliot.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "zbirenbaum/copilot.lua",
+        event = "InsertEnter",
+        opts = {
+            suggestion = { enabled = true, auto_trigger = true },
+            panel = { enabled = true },
+        },
+        config = function(_, opts)
+            require("copilot").setup(opts)
+        end,
+    },
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,3 +1,26 @@
 return {
-
+    {
+        "neovim/nvim-lspconfig",
+        opts = {
+            servers = {
+                clangd = {
+                    cmd = {
+                        "clangd",
+                        "--clang-tidy=false",
+                        "--background-index",
+                        "--query-driver=C:/msys64/mingw64/bin/g++.exe;C:/msys64/mingw64/bin/gcc.exe",
+                    },
+                    init_options = {
+                        -- put whichever flags you want to use by default
+                        fallbackFlags = require("lsp.clangd_ot").flags,
+                    },
+                    on_attach = function(client, _)
+                        client.server_capabilities.documentFormattingProvider = false
+                        client.server_capabilities.documentRangeFormattingProvider = false
+                    end,
+                    capabilities = { offsetEncoding = { "utf-16" } },
+                },
+            },
+        },
+    },
 }
