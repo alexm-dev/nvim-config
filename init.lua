f04d8775d6c9dcda34db875b0e370e4314dc7100
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2025-11-01 01:06:00 +0100

    Removed node24 binaries paths in init.lua
    - Not needed since node 24 (lts) is managed by scoop

--- init.lua
+++ init.lua
@@ -1,11 +1,3 @@
--- Node24 binaries to path
-if vim.env.LOCALAPPDATA and vim.loop.fs_stat(vim.env.LOCALAPPDATA .. "\\node24") then
-    local node_path = vim.env.LOCALAPPDATA .. "\\node24"
-    if not vim.env.PATH:find(node_path, 1, true) then
-        vim.env.PATH = node_path .. ";" .. (vim.env.PATH or "")
-    end
-end
-
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
