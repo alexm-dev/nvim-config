a090c8f1eded83b18a4cb90918c6b158481b6cd9
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2025-11-12 22:57:40 +0100

    Removed clangd setting
    - Not needed anymore clangd now handles it fine

--- init.lua
+++ init.lua
@@ -1,10 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- Clangd settings
-require("config.clangd_settings")
-require("lsp.hide_clangd_diagnostics")
-
 -- Colorscheme
 -- require("config.highlights")
 vim.highlight.priorities.semantic_tokens = 94
