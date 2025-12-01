08d943e4f10f939edab34015987098068b4af069
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2025-12-01 22:09:23 +0100

    Added statusline highlight color

--- lua/config/highlights.lua
+++ lua/config/highlights.lua
@@ -7,3 +7,6 @@ vim.api.nvim_set_hl(0, "DashBoardFooter", { fg = "#6f6f6f" })
 
 vim.api.nvim_set_hl(0, "NoiceCmdlineBorder", { fg = "#5c6370", bg = "#1f2335" })
 vim.api.nvim_set_hl(0, "NoiceCmdlinePopup", { fg = "#FFFFFF", bg = "#1f2335" })
+
+vim.api.nvim_set_hl(0, "StatusLine", { bg = "#161616" })
+vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#161616" })
