ba092093bd934ca41cdf0c25156d39d4daf9530e
Alexandros McCray <223855625+alexm-dev@users.noreply.github.com>
2025-12-02 14:55:09 +0100

    Made colorscheme background a bit darker

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,56 +7,6 @@ return {
         name = "catppuccin",
         opts = {
             flavour = "mocha", -- latte, frappe, macchiato, mocha
-            lsp_styles = {
-                underlines = {
-                    errors = { "undercurl" },
-                    hints = { "undercurl" },
-                    warnings = { "undercurl" },
-                    information = { "undercurl" },
-                },
-            },
-            integrations = {
-                aerial = true,
-                alpha = true,
-                cmp = true,
-                dashboard = true,
-                flash = true,
-                fzf = true,
-                grug_far = true,
-                gitsigns = true,
-                headlines = true,
-                illuminate = true,
-                indent_blankline = { enabled = true },
-                leap = true,
-                lsp_trouble = true,
-                mason = true,
-                mini = true,
-                navic = { enabled = true, custom_bg = "lualine" },
-                neotest = true,
-                neotree = true,
-                noice = true,
-                notify = true,
-                snacks = true,
-                telescope = true,
-                treesitter_context = true,
-                which_key = true,
-            },
-            custom_highlights = function(colors)
-                return {
-                    ["@lsp.type.parameter"] = { link = "@variable.parameter" },
-                }
-            end,
-        },
-        specs = {
-            {
-                "akinsho/bufferline.nvim",
-                optional = true,
-                opts = function(_, opts)
-                    if (vim.g.colors_name or ""):find("catppuccin") then
-                        opts.highlights = require("catppuccin.special.bufferline").get_theme()
-                    end
-                end,
-            },
         },
     },
 
@@ -64,7 +14,16 @@ return {
         "EdenEast/nightfox.nvim",
         priority = 1000,
         lazy = false,
-        opts = {},
+        opts = {
+            palettes = {
+                carbonfox = {
+                    bg1 = "#111111",
+                },
+            },
+        },
+        config = function(_, opts)
+            require("nightfox").setup(opts)
+        end,
     },
 
     -- Configure LazyVim to load colorscheme
